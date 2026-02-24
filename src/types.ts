656e33384eea926a5e9cb52a9a18982a933c85d2
EpicCarlito <jameschef2018@gmail.com>
2026-02-24 17:27:46 -0500

    forgot again, but view transitions might not be worth it

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,15 @@ export interface VideoParams {
   videoId: string;
   title: string;
 }
+
+export interface Season {
+  year: number;
+  seasonName: string;
+  src: string;
+  robot: {
+    name: string;
+    desc: string;
+    src?: string;
+    github?: string;
+  };
+}
